5c3cce6d422531e480977ec2a10daad48880515d
James Wilson <james@jsdw.me>
2025-07-30 15:29:51 +0100

    Turns out you can have too many constructor args, so remove for 2 tests

--- solidity/simple/storage/aligned/array/reassign_nested.sol
+++ solidity/simple/storage/aligned/array/reassign_nested.sol
@@ -3,13 +3,7 @@
 //!     "inputs": [
 //!         {
 //!             "method": "#deployer",
-//!             "instance": "Test",
-//!             "calldata": [
-//!                 "1", "2", "3", "4",
-//!                 "5", "6", "7", "8",
-//!                 "9", "10", "11", "12",
-//!                 "13", "14", "15", "16"
-//!             ]
+//!             "instance": "Test"
 //!         },
 //!         {
 //!             "method": "main",
@@ -28,16 +22,11 @@ contract Test {
 
     uint8 constant TEST = 42;
 
-    constructor(
-        uint256 a1, uint256 a2, uint256 a3, uint256 a4,
-        uint256 b1, uint256 b2, uint256 b3, uint256 b4,
-        uint256 c1, uint256 c2, uint256 c3, uint256 c4,
-        uint256 d1, uint256 d2, uint256 d3, uint256 d4
-    ) {
-        data[0][0] = a1; data[0][1] = a2; data[0][2] = a3; data[0][3] = a4;
-        data[1][0] = b1; data[1][1] = b2; data[1][2] = b3; data[1][3] = b4;
-        data[2][0] = c1; data[2][1] = c2; data[2][2] = c3; data[2][3] = c4;
-        data[3][0] = d1; data[3][1] = d2; data[3][2] = d3; data[3][3] = d4;
+    constructor() {
+        data[0][0] = 1;  data[0][1] = 2;  data[0][2] = 3;  data[0][3] = 4;
+        data[1][0] = 5;  data[1][1] = 6;  data[1][2] = 7;  data[1][3] = 8;
+        data[2][0] = 9;  data[2][1] = 10; data[2][2] = 11; data[2][3] = 12;
+        data[3][0] = 13; data[3][1] = 14; data[3][2] = 15; data[3][3] = 16;
     }
 
     function main(uint8 argument) public returns(uint8) {
